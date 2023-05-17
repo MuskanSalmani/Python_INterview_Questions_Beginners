1a17ff298a1600528d1bfe34e2d7f0899c20c9c2
Muskan <96484516+MuskanSalmani@users.noreply.github.com>
2023-05-17 21:02:02 +0530

    Add files via upload

---
+++ concatenate tuple.py
@@ -0,0 +1,14 @@
+
+"""
+Author = Muskan
+date = 17/may/2023
+
+When it is required to concatenate multiple tuples, the '+' operator can be used.
+A tuple is an immutable data type. It means, values once defined can't be changed
+by accessing their index elements
+"""
+
+tpl1 = (1,2,3,4,5,6,7,8,9,10)
+tpl2 = (11,12,13,14,15,16,17,18,19,20)
+
+print(tpl1+tpl2)
