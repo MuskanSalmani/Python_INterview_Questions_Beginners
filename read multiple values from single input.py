b41383b895cd74d1467abed8bf7f27cf7cace7d1
Muskan <96484516+MuskanSalmani@users.noreply.github.com>
2023-05-18 20:26:42 +0530

    Add files via upload

---
+++ read multiple values from single input.py
@@ -0,0 +1,13 @@
+# by using split
+
+# x1 = list(map(int,input("Enter value").split()))
+# print(x1)
+
+
+
+# x2 = [int(x) for x in input("Enter value").split()]
+# print(x2)
+
+
+x3 = [int(x) for x in input("Enter value").split(",")]
+print(x3)
