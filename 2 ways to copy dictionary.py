d0adacbee7e79606247c528328184b9533cbdab3
Muskan <96484516+MuskanSalmani@users.noreply.github.com>
2023-05-18 20:44:04 +0530

    Add files via upload

---
+++ 2 ways to copy dictionary.py
@@ -0,0 +1,23 @@
+"""
+Author = Muskan
+date = 18/may/2023
+"""
+
+
+# Way - 1
+diction1 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "launch_year": 1964,
+  "current_year": 2023
+}
+
+diction2 = diction1.copy()
+
+print(diction2)
+
+
+# Way -2
+
+diction3 = diction1
+print(diction3)
