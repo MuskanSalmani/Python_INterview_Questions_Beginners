9b3f75dbd3669ad64ecb6504fc11f26938f68c06
Muskan <96484516+MuskanSalmani@users.noreply.github.com>
2023-05-18 20:52:27 +0530

    Add files via upload

---
+++ anonymous vs lambda function.py
@@ -0,0 +1,13 @@
+"""
+Author = Muskan
+date = 18/may/2023
+"""
+
+# Lambda Function it has a name for eg. cube
+
+cube = lambda x: x*x*x
+print(cube(2))
+
+
+# Anonymous Function it is not assigned to any variable that is why it is called anonymous as it has no name
+print((lambda x: x*x*x)(4))
